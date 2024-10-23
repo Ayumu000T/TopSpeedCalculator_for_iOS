b989bb91a05ab79e811555f5896951c4fe71b260
Ayumu <ic21at@gmail.com>
2024-10-23 11:39:02 +0900

    Initial Commit

---
+++ TopSpeedCalculator_for_iOS/TopSpeedCalculator_for_iOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  TopSpeedCalculator_for_iOSApp.swift
+//  TopSpeedCalculator_for_iOS
+//
+//  Created by Ayumu Takahashi on 2024/10/23.
+//
+
+import SwiftUI
+
+@main
+struct TopSpeedCalculator_for_iOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TopSpeedCalculator_for_iOSTests/TopSpeedCalculator_for_iOSTests.swift
@@ -0,0 +1,17 @@
+//
+//  TopSpeedCalculator_for_iOSTests.swift
+//  TopSpeedCalculator_for_iOSTests
+//
+//  Created by Ayumu Takahashi on 2024/10/23.
+//
+
+import Testing
+@testable import TopSpeedCalculator_for_iOS
+
+struct TopSpeedCalculator_for_iOSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}

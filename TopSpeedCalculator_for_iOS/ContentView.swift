16cf1a6370aacc8791eaab436bcda34f38eb0e93
Ayumu <ic21at@gmail.com>
2024-10-23 11:51:30 +0900

    First commit

--- TopSpeedCalculator_for_iOS/ContentView.swift
+++ TopSpeedCalculator_for_iOS/ContentView.swift
@@ -1,21 +1,25 @@
-//
-//  ContentView.swift
-//  TopSpeedCalculator_for_iOS
-//
-//  Created by Ayumu Takahashi on 2024/10/23.
-//
-
 import SwiftUI
+
+struct AppColors {
+    static let backgroundColor1 = Color(red: 0.313, green: 0.639, blue: 0.671) // 背景色1(ターコイズブルー)
+    static let backgroundColor2 = Color(red: 0.98, green: 0.86, blue: 0.76) //　背景色2(クリーム)
+    static let textColor = Color(red: 29/255, green: 49/255, blue: 60/255) // 文字色
+}
+
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            AppColors.backgroundColor1 //背景色
+                .ignoresSafeArea()
+
+            VStack {
+                Rectangle()
+                    .fill(AppColors.backgroundColor2)
+                    .frame(width: 300, height: 300)
+                    
+            }
         }
-        .padding()
     }
 }
 
